b3a6b0fb2d280695b7c8c0a64bed3a25e75ccf28
daivanduong <dai22992@gmail.com>
2025-09-20 16:36:06 +0700

    Initial Commit

---
+++ NotesApp-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotesApp-MVVM
+//
+//  Created by Đại Dương on 20/09/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
